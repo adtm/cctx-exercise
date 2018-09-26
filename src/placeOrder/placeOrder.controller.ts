f40131feca721c93e80c8dcb8db55038cbc9d12c
adtm <tomas.eglinskas@gmail.com>
2018-09-26 22:30:21 +0300

    clean(): fix scripts, remove console.logs

--- src/placeOrder/placeOrder.controller.ts
+++ src/placeOrder/placeOrder.controller.ts
@@ -3,8 +3,6 @@ import { Context } from "koa";
 import * as placeOrderService from "./placeOrder.service";
 
 const placeOrder = async (ctx: Context) => {
-  console.log(ctx.query);
-  console.log("---");
   ctx.body = await placeOrderService.placeOrder(ctx.query);
 };
 
